293234c8bc77d781738b24598bdbfbced9cbda7c
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2023-06-15 14:25:10 +0530

    Updated conf.py

--- conf.py
+++ conf.py
@@ -22,13 +22,13 @@ author = "K.Srinivasan"
 # -- General configuration ---------------------------------------------------
 # -- General configuration
 
-extensions = [
-    "sphinx.ext.duration",
-    "sphinx.ext.doctest",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.intersphinx",
-]
+#extensions = [
+#    "sphinx.ext.duration",
+#    "sphinx.ext.doctest",
+#    "sphinx.ext.autodoc",
+#    "sphinx.ext.autosummary",
+#    "sphinx.ext.intersphinx",
+#]
 
 intersphinx_mapping = {
     "rtd": ("https://docs.readthedocs.io/en/stable/", None),

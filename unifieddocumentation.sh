5fed96103ec8e367e056f76fb3cd73b1c15156a0
K.Srinivasan <ksrinivasan@krishna-iresearch.org>
2024-10-24 14:31:37 +0530

    NeuronRain Unified Documentation eBook: --------------------------------------- (*) New bash script unifieddocumentation.sh has been added to repositories for collating all the text design documents across each NeuronRain repositories and create a single NeuronRainDesign_unified.txt file (*) New postscript document format has been added by converting NeuronRainDesign_unified.text to NeuronRainDesign_unified.ps (instead of pdf because of huge size and slowdown of almost all conversion utilities - pandoc,enscript,libreoffice - commandlines for pandoc and enscript have been included in the script but commented) (*) Postscript document ebook NeuronRainDesign_unified.ps has been compressed and added to repositories under ps/ (*) Due to huge number of pages in unified documentation postscript ebook (~50000 pages), postscript reader is a prerequisite.

---
+++ unifieddocumentation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+rm -rf pdf
+rm -rf text
+git checkout pdf
+git checkout text
+cat /dev/null > text/NeuronRain_Documents/NeuronRainDesign_unified.txt
+for i in `find .. -name "*.txt"`
+do
+	cp $i text/NeuronRain_Documents/
+done
+for i in `ls text/NeuronRain_Documents/*`
+do
+	echo "-------------------------------------" >> text/NeuronRainDesign_unified.txt
+	echo $i >> text/NeuronRainDesign_unified.txt
+	echo "-------------------------------------" >> text/NeuronRainDesign_unified.txt
+	cat $i >> text/NeuronRainDesign_unified.txt
+done
+mv text/NeuronRainDesign_unified.txt text/NeuronRain_Documents/NeuronRainDesign_unified.txt
+echo "--------------------------------------------------" >> text/NeuronRain_Documents/NeuronRainDesign_unified.txt
+echo "text/NeuronRain_Documents/index.rst" >> text/NeuronRain_Documents/NeuronRainDesign_unified.txt 
+echo "--------------------------------------------------" >> text/NeuronRain_Documents/NeuronRainDesign_unified.txt
+cat index.rst >> text/NeuronRain_Documents/NeuronRainDesign_unified.txt 
+#pandoc text/NeuronRain_Documents/NeuronRainDesign_unified.txt --pdf-engine wkhtmltopdf -o pdf/NeuronRain_Documents/NeuronRainDesign_unified.pdf 
+#enscript text/NeuronRain_Documents/NeuronRainDesign_unified.txt -o - | ps2pdf  - pdf/NeuronRain_Documents/NeuronRainDesign_unified.pdf
+a2ps -v text/NeuronRain_Documents/NeuronRainDesign_unified.txt --output=text/NeuronRain_Documents/NeuronRainDesign_unified.ps
+mv text/NeuronRain_Documents/NeuronRainDesign_unified.ps ps/

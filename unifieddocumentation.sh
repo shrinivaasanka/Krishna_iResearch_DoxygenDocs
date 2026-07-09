534882f6c2e57a23d3e0b20aabca7a4e34a8c67a
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2026-07-09 14:00:37 +0530

    Updated Unified NeuronRain Documentation

--- unifieddocumentation.sh
+++ unifieddocumentation.sh
@@ -20,11 +20,12 @@ echo "--------------------------------------------------" >> text/NeuronRain_Doc
 echo "text/NeuronRain_Documents/index.rst" >> text/NeuronRain_Documents/NeuronRainDesign_unified.txt 
 echo "--------------------------------------------------" >> text/NeuronRain_Documents/NeuronRainDesign_unified.txt
 cat index.rst >> text/NeuronRain_Documents/NeuronRainDesign_unified.txt 
+#soffice --headless --convert-to pdf text/NeuronRain_Documents/NeuronRainDesign_unified.txt 
 #pandoc text/NeuronRain_Documents/NeuronRainDesign_unified.txt --pdf-engine wkhtmltopdf -o pdf/NeuronRain_Documents/NeuronRainDesign_unified.pdf 
 #enscript text/NeuronRain_Documents/NeuronRainDesign_unified.txt -o - | ps2pdf  - pdf/NeuronRain_Documents/NeuronRainDesign_unified.pdf
 #a2ps -v text/NeuronRain_Documents/NeuronRainDesign_unified.txt --output=text/NeuronRain_Documents/NeuronRainDesign_unified.ps
 #mv text/NeuronRain_Documents/NeuronRainDesign_unified.ps ps/
-#pandoc index.rst -o index.html
-rst-buildhtml --report=5
+pandoc index.rst -o index.html
+#rst-buildhtml --report=5
 #pandoc index.html --pdf-engine wkhtmltopdf -o pdf/NeuronRain_Documents/neuronrain-documentation.pdf 
-rst2pdf index.rst -o pdf/NeuronRain_Documents/neuronrain-documentation.pdf 
+#rst2pdf index.rst -o pdf/NeuronRain_Documents/neuronrain-documentation.pdf 
